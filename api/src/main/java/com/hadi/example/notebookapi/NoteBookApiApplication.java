ebf880599065c1f4b5fca60bfeabb851dff6db47
h.moradi <hadi000333@gmail.com>
2024-04-11 16:58:27 +0330

    first spring initialize commit

---
+++ api/src/main/java/com/hadi/example/notebookapi/NoteBookApiApplication.java
@@ -0,0 +1,13 @@
+package com.hadi.example.notebookapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteBookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteBookApiApplication.class, args);
+	}
+
+}
